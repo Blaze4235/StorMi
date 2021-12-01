8abad7b5cd1b68eb1aaa5e628b7cdf3c087727b8
Andrii Fedin <andrii.fedin@nure.ua>
2021-12-01 23:14:29 +0200

    Init commit

---
+++ server/StorMi/Controllers/AccountController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StoMi.ViewModels;
+using StorMi.DalModels;
+using StorMi.EF;
+using StorMi.DalModels;
+using StorMi.ViewModels;
+
+namespace AuthApp.Controllers
+{
+    [Route("/api")]
+    public class AccountController : Controller
+    {
+        private readonly StormiContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            StormiContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _db = context;
+        }
+
+        [HttpGet]
+        [Route("/users")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(await _db.UserProfiles.ToListAsync());
+        }
+
+        //[HttpGet]
+        //public IActionResult Register()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        [Route("/register")]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+
+                // добавляем пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    return Json("Success");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    return BadRequest();
+                }
+            }
+            return StatusCode(500);
+        }
+
+        [HttpPost]
+        [Route("/login")]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    // проверяем, принадлежит ли URL приложению
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return Json("Success");
+                    }
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
