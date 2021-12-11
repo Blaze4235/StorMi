024e0aa17f71b8659301fad3bd7972172be41e7b
dmytrovorona <dmytro.vorona1@nure.ua>
2021-12-11 15:27:21 +0200

    Create test weather model

---
+++ server/StorMi/Models/WeatherModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoMi.ViewModels
+{
+    public class WeatherModel
+    {
+        public string Name { get; set; }
+
+        public int Temperature { get; set; }
+
+        public int TemperatureMin { get; set; }
+
+        public int TemperatureMax { get; set; }
+
+        public float WindSpeed { get; set; }
+
+        public float WindDeg { get; set; }
+
+        public string WindGust { get; set; }
+
+        public string Rain { get; set; }
+
+        public string Snow { get; set; }
+
+        public int CloundsAll { get; set; }
+
+    }
+}
