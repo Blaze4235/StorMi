a7398b7f6b4a47c4b46519f0903a1f812b10db13
dmytrovorona <dmytro.vorona1@nure.ua>
2021-12-11 17:44:11 +0200

    Add week weather forecast query

--- server/StorMi/Controllers/WeatherController.cs
+++ server/StorMi/Controllers/WeatherController.cs
@@ -16,8 +16,8 @@ namespace StorMi.Controllers
 
 
         [HttpGet]
-        [Route("/weather")]
-        public async Task<IActionResult> WeatherForecast()
+        [Route("/weather/day")]
+        public async Task<IActionResult> DayWeatherForecast()
         {
             // First API: https://api.weatherapi.com
             dynamic res = null;
@@ -33,9 +33,9 @@ namespace StorMi.Controllers
                     var location = res["location"];
                     var forecast = res["forecast"];
                     var forecastDay = forecast["forecastday"];
-                    var hour = forecastDay[0];
+                    var day = forecastDay[0];
 
-                    var weatherParams = hour["day"];
+                    var weatherParams = day["day"];
                     //var weatherParams = hour["day"].avghumidity;
                     var condition = weatherParams["condition"];
 
@@ -49,6 +49,51 @@ namespace StorMi.Controllers
                     model.TemperatureMax = Convert.ToInt32(weatherParams["maxtemp_c"]);
                     model.WindSpeed = weatherParams["maxwind_kph"];
                     model.OverallCondition = weatherParams["condition"]?["text"];
+                }
+            }
+
+            return Json(model);
+        }
+
+        [HttpGet]
+        [Route("/weather/week")]
+        public async Task<IActionResult> WeekWeatherForecast()
+        {
+            // First API: https://api.weatherapi.com
+            dynamic res = null;
+            List<WeatherModelDay> model = new List<WeatherModelDay>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://api.weatherapi.com/v1/forecast.json?key=2f3b18240aca47f9a5e131759211112&q=Kharkiv&days=7&aqi=no&alerts=no"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    //dynamic data = System.Web.Helpers.Json.Decode(apiResponse);
+                    res = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(apiResponse);
+                    var location = res["location"];
+                    var forecast = res["forecast"];
+                    var forecastDay = forecast["forecastday"];
+                    for (int i = 0; i < forecastDay.Count; i++)
+                    {
+                        var node = new WeatherModelDay();
+                        var day = forecastDay[i];
+                        var weatherParams = day["day"];
+                        //var weatherParams = hour["day"].avghumidity;
+                        var condition = weatherParams["condition"];
+
+                        node.City = location["name"];
+                        node.Country = location["country"];
+                        node.AvgHumidity = weatherParams["avghumidity"];
+                        node.AvgTemp = weatherParams["avgtemp_c"];
+                        node.ChanceOfRain = weatherParams["daily_chance_of_rain"];
+                        node.ChanceOfSnow = weatherParams["daily_chance_of_snow"];
+                        node.TemperatureMin = Convert.ToInt32(weatherParams["mintemp_c"]);
+                        node.TemperatureMax = Convert.ToInt32(weatherParams["maxtemp_c"]);
+                        node.WindSpeed = weatherParams["maxwind_kph"];
+                        node.OverallCondition = weatherParams["condition"]?["text"];
+
+                        model.Add(node);
+                    }
                 }
             }
 
---
+++ server/StorMi/Models/WeatherAPI/WeatherModelHour.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorMi.Models.WeatherAPI
+{
+    public class WeatherModelHour
+    {
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public float Humidity { get; set; }
+
+        public int Temp { get; set; }
+
+        public int FeelsLikeTemp { get; set; }
+
+        public float ChanceOfRain { get; set; }
+
+        public float ChanceOfSnow { get; set; }
+
+        public float WindSpeed { get; set; }
+
+        public float WindDegree { get; set; }
+
+        public string WindDirection { get; set; }
+
+        public string AvgVisibilityInKm { get; set; }
+
+        public string WindGustInKm { get; set; }
+
+        public string OverallCondition { get; set; }
+    }
+}
